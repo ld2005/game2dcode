d5496388e9b1779fd619992f2309ebf40cec25de
ld2005 <lam732399@gmail.com>
2026-02-03 20:21:42 +0700

    create script Health

---
+++ Assets/Health.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public GameObject explosionPrefab;
+
+    public void OnTriggerEnter2D(Collider2D collision) => Die();
+
+    protected virtual void Die()
+    {
+        var explosion = Instantiate(explosionPrefab, transform.position,
+        transform.rotation);
+        Destroy(explosion, 1);
+        Destroy(gameObject);
+    }
+}
