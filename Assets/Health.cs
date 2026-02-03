f0e773941c511008a36c4d4e8810a3137af04a9f
ld2005 <lam732399@gmail.com>
2026-02-03 20:30:12 +0700

    MOVE REUSABLE CONTENT TO HEALTH

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -1,17 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
     public GameObject explosionPrefab;
+    public int defaultHealthPoint;
+    private int healthPoint;
 
-    public void OnTriggerEnter2D(Collider2D collision) => Die();
+    private void Start() => healthPoint = defaultHealthPoint;
 
+    public void TakeDamage(int damage)
+    {
+        if (healthPoint <= 0) return;
+
+        healthPoint -= damage;
+        if (healthPoint <= 0) Die();
+    }
     protected virtual void Die()
     {
-        var explosion = Instantiate(explosionPrefab, transform.position,
-        transform.rotation);
+        var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
         Destroy(explosion, 1);
         Destroy(gameObject);
     }
